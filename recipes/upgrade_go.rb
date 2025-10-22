cc145fbc6a46e7b4c827e8a3d23c68a1c7ca865d
sue445 <sue445@sue445.net>
2025-10-23 00:08:14 +0900

    Add upgrade_go.rb

---
+++ recipes/upgrade_go.rb
@@ -0,0 +1,30 @@
+@go_minor_version = params[:go_version].to_f
+
+v = params[:go_version].split(".")
+@gcp_runtime_version = "go#{v[0]}#{v[1]}"
+
+update_file ".github/workflows/*.yml" do |content|
+  content.gsub!(/go-version:\s+[\d.]+\s*$/, "go-version: #{params[:go_version]}")
+  content.gsub!(/GO_VERSION:\s+[\d.]+$/, "GO_VERSION: #{params[:go_version]}")
+  content.gsub!(/GOLANGCI_LINT_VERSION: v[0-9.]+/, "GOLANGCI_LINT_VERSION: #{params[:golangci_lint_version]}")
+
+  content.gsub!(/go\d{3}(?!\d)/, @gcp_runtime_version )
+end
+
+update_file "app.yaml" do |content|
+  content.gsub!(/go\d{3}(?!\d)/, @gcp_runtime_version )
+end
+
+update_file "go.mod" do |content|
+  content.gsub!(/^go [\d.]+$/, "go #{params[:go_version]}")
+end
+
+update_file ".circleci/config.yml" do |content|
+  content.gsub!(%r{- image: circleci/golang:[\d.]+}, "- image: circleci/golang:#{@go_minor_version}")
+  content.gsub!(%r{- image: cimg/go:[\d.]+}, "- image: cimg/go:#{@go_minor_version}")
+end
+
+
+update_file "Dockerfile" do |content|
+  content.gsub!(/^FROM golang:([\d.]+)/, %Q{FROM golang:#{@go_minor_version}})
+end
