d32be9eba327afa303b9da33bac0d71306a713c7
sue445 <sue445@sue445.net>
2025-11-02 01:38:52 +0900

    Update dependabot.yml

--- recipes/ruby_gem.rb
+++ recipes/ruby_gem.rb
@@ -2,3 +2,8 @@ update_file ".github/workflows/rbs-collection-updater.yml" do |content|
   file = File.read(File.join(__dir__, "files", "ruby_gem", "rbs-collection-updater.yml"))
   content.replace(file)
 end
+
+update_file ".github/dependabot.yml" do |content|
+  file = File.read(File.join(__dir__, "files", "ruby_gem", "dependabot.yml"))
+  content.replace(file)
+end
