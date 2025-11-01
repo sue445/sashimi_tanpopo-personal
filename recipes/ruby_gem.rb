ef980b79633990af644c28ce48853f2b5cd1378a
sue445 <sue445@sue445.net>
2025-11-02 01:01:26 +0900

    Add recipe

---
+++ recipes/ruby_gem.rb
@@ -0,0 +1,4 @@
+update_file ".github/workflows/rbs-collection-updater.yml" do |content|
+  file = File.read(File.join(__dir__, "files", "ruby_gem", "rbs-collection-updater.yml"))
+  content.replace(file)
+end
