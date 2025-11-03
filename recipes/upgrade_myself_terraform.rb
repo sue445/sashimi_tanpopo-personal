85ddd23d4b1aed292690ef1509a43094d89e2a22
sue445 <sue445@sue445.net>
2025-11-03 10:51:47 +0900

    upgrade_terraform -> terraform_upgrade_version

--- recipes/upgrade_myself_terraform.rb
+++ recipes/upgrade_myself_terraform.rb
@@ -2,6 +2,6 @@ require_relative "../misc/terraform_helper"
 
 @latest_terraform_version = TerraformHelper.latest_terraform_version
 
-update_file ".github/workflows/upgrade_terraform.yml" do |content|
+update_file ".github/workflows/terraform_upgrade_version.yml" do |content|
   content.gsub!(%r(terraform_version: +".+"), %Q(terraform_version: "#{@latest_terraform_version}"))
 end
