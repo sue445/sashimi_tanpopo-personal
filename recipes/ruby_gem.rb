54cee4b85852b6d4c8cabfea22bf9c83017e15ad
sue445 <sue445@sue445.net>
2025-12-01 23:34:12 +0900

    Remove files/ruby_gem/rbs-collection-updater.yml

--- recipes/ruby_gem.rb
+++
@@ -1,5 +0,0 @@
-# FIXME: Do after
-# update_file ".github/workflows/rbs-collection-updater.yml" do |content|
-#   file = File.read(File.join(__dir__, "files", "ruby_gem", "rbs-collection-updater.yml"))
-#   content.replace(file)
-# end
